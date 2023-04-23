344497990bbfba261c4b7429a54003bc41122ba1
Tobi Bolarinwa <2058402@csl-student.wlv.ac.uk>
2023-04-23 11:56:12 +0100

    msdfds

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ class App extends React.Component {
 	    this.handleDelete = this.handleDelete.bind(this);
   }
 	// get rich quick
-	
+	// saving God
 // Api is to fetch the data
 	componentDidMount() {
 	fetch('https://mi-linux.wlv.ac.uk/~2058402/webproject/public/index.php/BookController/index')
