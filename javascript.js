aaae9b13f3fbdd796f0d1c0871e11bc67c2e491e
Tobi Bolarinwa <2058402@csl-student.wlv.ac.uk>
2023-04-28 12:39:40 +0100

    msdfds

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ class App extends React.Component {
 	    this.handleDelete = this.handleDelete.bind(this);
   }
 
-// the mouse is out of the bag
+// the black bag
 
 	componentDidMount() {
 	fetch('https://mi-linux.wlv.ac.uk/~2058402/webproject/public/index.php/BookController/index')
