a6cb3b10429541228dbaca4ebe7a00042c4f8b3d
Tobi Bolarinwa <2058402@csl-student.wlv.ac.uk>
2023-05-14 05:33:58 +0100

    msdfds

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ class App extends React.Component {
 	    this.handleDelete = this.handleDelete.bind(this);
   }
 
-// going home not too far
+// am trying my best 
 
 	componentDidMount() {
 	fetch('https://mi-linux.wlv.ac.uk/~2058402/webproject/public/index.php/BookController/index')
